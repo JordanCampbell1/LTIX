3f409c738282a699b7b4283defba51835ce631b7
Jordan Campbell <jordancampbell.work@gmail.com>
2026-02-02 02:37:15 -0500

    initial setup

---
+++ reporting-service/src/main/java/com/example/demo/LtixApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtixApplication.class, args);
+	}
+
+}
